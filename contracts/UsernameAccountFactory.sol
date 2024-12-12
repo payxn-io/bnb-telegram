a235249f4a8751bed9600a4deaf5aeedf9e504fc
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-12 10:39:01 -0800

    🔶 Add inheritance is BaseAccountFactory

--- contracts/UsernameAccountFactory.sol
+++ contracts/UsernameAccountFactory.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import '@thirdweb-dev/contracts/prebuilts/account/utils/BaseAccountFactory.sol';
 import './UsernameAccount.sol';
 
-contract UsernameAccountFactory {
+contract UsernameAccountFactory is BaseAccountFactory {
 
     event RegisteredUsername(string username, address account);
     mapping(string => address) public accountOfUsername;
