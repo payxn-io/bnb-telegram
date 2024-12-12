e02ac5cc76c6870cc795b1dc0f86b57f6a710cf8
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-11 23:28:12 -0800

    🔶 Imported base account

--- contracts/UsernameAccount.sol
+++ contracts/UsernameAccount.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.9;
 
 import './UsernameAccountFactory.sol';
-import '@thirdweb-dev/contracts/prebuilts/account/non-upgradeable/Account.sol';
 
 contract UsernameAccount {
     constructor() {}
