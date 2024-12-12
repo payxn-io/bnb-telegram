11b190df0774de0c7653a0f6b316c3b9e728e077
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-12 10:53:50 -0800

    🔶 Change the solidity version

--- contracts/UsernameAccount.sol
+++ contracts/UsernameAccount.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.13;
 
 import './UsernameAccountFactory.sol';
 import '@thirdweb-dev/contracts/prebuilts/account/non-upgradeable/Account.sol';
--- contracts/UsernameAccountFactory.sol
+++ contracts/UsernameAccountFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.13;
 
 import '@thirdweb-dev/contracts/prebuilts/account/utils/BaseAccountFactory.sol';
 import './UsernameAccount.sol';
