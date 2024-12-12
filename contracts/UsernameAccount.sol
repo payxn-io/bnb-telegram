fb4e6f04a025799222fccb25f422525612e6f0e7
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-11 23:27:46 -0800

    🔶 Imported UsernameAccountFactory

--- contracts/UsernameAccount.sol
+++ contracts/UsernameAccount.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import './UsernameAccountFactory.sol';
+import '@thirdweb-dev/contracts/prebuilts/account/non-upgradeable/Account.sol';
+
 contract UsernameAccount {
     constructor() {}
 }
