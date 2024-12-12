be4b53ea3ad70687e276e883917c2c1c8abb8690
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-12 00:25:23 -0800

    🔶 Import BaseAccountFactory

--- contracts/UsernameAccountFactory.sol
+++ contracts/UsernameAccountFactory.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import '@thirdweb-dev/contracts/prebuilts/account/utils/BaseAccountFactory.sol';
+
 contract UsernameAccountFactory {
     constructor() {}
 }
